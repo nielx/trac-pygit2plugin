31b65f638b5ca05fdd0c487ef75da0ea8ff8d3e2
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-11-28 16:48:37 +0000

    TracPygit2Plugin: call `setup()` only if run setup.py as a script

--- tracpygit2plugin/trunk/setup.py
+++ tracpygit2plugin/trunk/setup.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+if __name__ == '__main__':
+    from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages
-from pkg_resources import parse_version
+    extra = {}
 
-extra = {}
-
-setup(
-    name='TracPygit2Plugin',
-    version='0.12.0.1',
-    description='Pygit2 integration for Git repository on Trac 0.12+',
-    license='BSD',  # the same as Trac
-    url='http://trac-hacks.org/wiki/TracPygit2Plugin',
-    author='Jun Omae',
-    author_email='jun66j5@gmail.com',
-    packages=find_packages(exclude=['*.tests*']),
-    package_data={},
-    test_suite='tracext.pygit2.tests.suite',
-    zip_safe=True,
-    install_requires=['Trac', 'pygit2'],
-    entry_points={
-        'trac.plugins': [
-            'tracext.pygit2.git_fs = tracext.pygit2.git_fs',
-        ],
-    },
-    **extra)
+    setup(
+        name='TracPygit2Plugin',
+        version='0.12.0.1',
+        description='Pygit2 integration for Git repository on Trac 0.12+',
+        license='BSD',  # the same as Trac
+        url='http://trac-hacks.org/wiki/TracPygit2Plugin',
+        author='Jun Omae',
+        author_email='jun66j5@gmail.com',
+        packages=find_packages(exclude=['*.tests*']),
+        package_data={},
+        test_suite='tracext.pygit2.tests.suite',
+        zip_safe=True,
+        install_requires=['Trac', 'pygit2'],
+        entry_points={
+            'trac.plugins': [
+                'tracext.pygit2.git_fs = tracext.pygit2.git_fs',
+            ],
+        },
+        **extra)
